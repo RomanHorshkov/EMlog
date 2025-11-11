880ffb0bed33089a457ccf31212367d91dec9f2a
Roman Horshkov <roman.horshkov@gmail.com>
2025-11-11 23:50:45 +0100

    format things

--- tests/unit/unit_tests.h
+++ tests/unit/unit_tests.h
@@ -11,21 +11,22 @@
 #define TESTS_UNIT_UNIT_TESTS_H
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 /* set_level test */
-void emlog_set_level_simple(void **state);
+void emlog_set_level_simple(void** state);
 
 /* emlog_init tests */
-void emlog_init_default_env(void **state);
-void emlog_init_explicit_dbg(void **state);
-void emlog_init_env_parsing(void **state);
+void emlog_init_default_env(void** state);
+void emlog_init_explicit_dbg(void** state);
+void emlog_init_env_parsing(void** state);
 
 /* timestamp tests */
-void test_emlog_timestamps_true(void **state);
-void test_emlog_timestamps_false(void **state);
-void test_emlog_timestamps_toggle(void **state);
+void test_emlog_timestamps_true(void** state);
+void test_emlog_timestamps_false(void** state);
+void test_emlog_timestamps_toggle(void** state);
 
 #ifdef __cplusplus
 }
