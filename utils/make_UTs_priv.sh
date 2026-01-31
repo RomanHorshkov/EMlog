968d098047ba44321e25fa866755497c1c8ec312
Roman Horshkov <124358264+RomanHorshkov@users.noreply.github.com>
2026-01-31 17:11:57 +0100

    Added coverage compilation options

--- utils/make_UTs_priv.sh
+++ utils/make_UTs_priv.sh
@@ -17,6 +17,6 @@ BUILD_DIR="build/UTs"
 
 mkdir -p $BUILD_DIR
 
-# build without optimization and debug options
-gcc -std=c11 -O0 -g -I. -Iapp -c tests/UTs/test_private.c -o $BUILD_DIR/test_private.o
-gcc $BUILD_DIR/test_private.o -o $BUILD_DIR/ut_private -lcmocka -pthread
+# build without optimization and debug options and coverage option
+gcc -std=c11 -O0 -g --coverage -I. -Iapp -c tests/UTs/test_private.c -o $BUILD_DIR/test_private.o
+gcc --coverage $BUILD_DIR/test_private.o -o $BUILD_DIR/ut_private -lcmocka -pthread
