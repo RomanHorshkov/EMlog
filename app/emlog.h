9b40cbceb372ca3fd74430ebe19b7368e8162352
Roman Horshkov <124358264+RomanHorshkov@users.noreply.github.com>
2026-07-12 01:14:00 +0200

    Allow argument-free errno logging

--- app/emlog.h
+++ app/emlog.h
@@ -184,11 +184,11 @@ void emlog_log_errno(eml_level_t level, const char* comp, int err, const char* f
  *
  * Usage: EML_PERR("mod", "failed to open %s", path);
  */
-#define EML_PERR(tag, fmt, ...)                                         \
+#define EML_PERR(tag, ...)                                         \
     do                                                                  \
     {                                                                   \
         int __e = errno;                                                \
-        emlog_log_errno(EML_LEVEL_ERROR, tag, __e, fmt, ##__VA_ARGS__); \
+        emlog_log_errno(EML_LEVEL_ERROR, tag, __e, __VA_ARGS__); \
     } while(0)
 
 /**
